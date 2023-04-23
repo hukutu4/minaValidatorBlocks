2bed0603b647c9de751da2de8d7f9a27f68db335
hukutu4 <dxza@ya.ru>
2023-04-23 12:38:36 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python MinaExplorer Blocks Log Script
+
+This Python script retrieves the latest blocks data for a specified validator address from [MinaExplorer](https://minaexplorer.com) and logs them to a file. The script uses the following modules:
+
+- `requests`: to make HTTP requests to the MinaExplorer API
+- `json`: to parse JSON data from the API response and log data to a file
+- `os`: to read environment variables for the validator address and log file name
+- `dotenv`: to load environment variables from a `.env` file
+
+## Setup
+
+Before running the script, ensure that you have installed the required modules by running `pip install -r requirements.txt`. 
+
+Next, set the following environment variables in a `.env` file:
+
+- `VALIDATOR_ADDRESS`: the validator address for which to retrieve the blocks data
+- `LOG_FILE_NAME`: the name of the log file to which the data will be logged
+
+## Running the script
+
+To run the script, simply execute `python log_blocks.py` in the terminal. 
+
+The script first reads the StateHash of the last record in the log file (if it exists) and retrieves the latest blocks data from MinaExplorer. It then iterates over each block in the data array and logs the specified fields to the log file.
+
+If a StateHash match is found in the log file, the script stops logging new records, assuming that all records after the match have already been logged. If no StateHash match is found in the log file, the script logs all records from the response data.
---
+++ log_blocks.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# get the value of the environment variable
+validator_address = os.getenv("VALIDATOR_ADDRESS")
+
+# set the URL for getting data
+url = f"https://minaexplorer.com/all-blocks/{validator_address}?canonical=False&draw=1&start=0&length=10"
+
+# set the log file name
+log_file_name = os.getenv("LOG_FILE_NAME")
+
+# set the list of fields to output to the log file
+fields_to_output = ["DateTime", "Epoch", "Slot", "BlockchainLength", "Transactions", "Coinbase", "StateHash", "BuiltOn"]
+
+# find the StateHash of the last record in the log file
+try:
+    with open(log_file_name, "r") as f:
+        last_line = f.readlines()[-1]
+        last_state_hash = json.loads(last_line)["StateHash"]
+except (FileNotFoundError, IndexError):
+    # set last_state_hash to None if the file is not found or empty
+    last_state_hash = None
+
+# get data from the URL and parse JSON
+response = requests.get(url)
+data = json.loads(response.text)
+# reverse the order of elements in the array to have dates from old to new (URL returns them from new to old)
+data['data'].reverse()
+
+# iterate over each element in the data array
+found_last_state_hash = False
+for block in data['data']:
+    # create a dictionary with the fields to output to the log file
+    fields_dict = {k: v for k, v in block.items() if k in fields_to_output}
+    # get the value of the StateHash field from the current element
+    current_state_hash = block['StateHash']
+
+    if found_last_state_hash:
+        # add a new line to the log file
+        with open(log_file_name, "a") as f:
+            f.write(json.dumps(fields_dict) + "\n")
+    elif last_state_hash is None or current_state_hash == last_state_hash:
+        # start adding new lines to the log file if last_state_hash is not set (file is empty),
+        # or if a StateHash match is found
+        found_last_state_hash = True
+
+# if no StateHash match is found in the log file, add all records from the response data to the log file
+if not found_last_state_hash:
+    with open(log_file_name, "a") as f:
+        for block in data['data']:
+            # create a dictionary with the fields to output to the log file
+            fields_dict = {k: v for k, v in block.items() if k in fields_to_output}
+            f.write(json.dumps(fields_dict) + "\n")
